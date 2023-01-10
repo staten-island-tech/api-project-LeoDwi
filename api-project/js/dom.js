f003c992305abe93a29d61d8e742f539344695c9
LeoDwi <leod13579@gmail.com>
2023-01-10 10:15:35 -0500

    Update dom.js

--- api-project/js/dom.js
+++ api-project/js/dom.js
@@ -1,4 +1,12 @@
-const DOMSelectors = {
+const ApiLinks = {
   today: "https://my_host/calendar/day/",
   input: "https://my_host/calendar/day/2018-06-09/",
 };
+
+const DOMSelectors = {
+  day: document.getElementById("day"),
+  month: document.getElementById("month"),
+  year: document.getElementById("year"),
+};
+
+const Card = {};
