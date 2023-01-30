416cf176b82cadc0ad2ecffc3e3f07aaea1c7b5a
LeoDwi <leod13579@gmail.com>
2023-01-30 12:40:58 -0500

    seventh commit

--- api-project/js/dom.js
+++ api-project/js/dom.js
@@ -2,6 +2,9 @@ export const DOMSelectors = {
   input: document.getElementById("breed"),
   button: document.getElementById("btn"),
   parent: document.getElementById("cards"),
+  errorHTML: `<div class="error"><h2 class="notfound">Sorry! We couldn't find what you were looking for!</h2>
+  <p>Please check your input for any spelling or grammatical mistakes. Otherwise, we do not have this breed of dog on hand.</p>
+  </div>`,
 };
 
 export const ApiLinks = {
--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,13 +1,13 @@
 import "../styles/style.css";
 import { ApiLinks, DOMSelectors } from "./dom";
 
-function insertHTML(x, y) {
+function insertHTML(input, image) {
   DOMSelectors.parent.insertAdjacentHTML(
-    "afterend",
+    "beforeend",
     `
   <div class="card">
-  <h1 class=dog-class>${x.Breeds}</h1>
-  <img class="display-image" src="${y.image}" alt="display image">
+  <h1 class=dog-class>${input}</h1>
+  <img class="display-image" src="${image}" alt="display image">
   </div>`
   );
 }
@@ -20,7 +20,14 @@ async function getData(URL) {
   try {
     const response = await fetch(URL);
     const object = await response.json();
-    const data = Object.values(object)[1];
+    const Data = object.message;
+    const primaryBreeds = Object.keys(Data);
+    console.log(primaryBreeds);
+    const subBreeds = Object.values(Data).filter(
+      (object) => object.length >= 1
+    );
+    console.log(subBreeds);
+    const data = primaryBreeds.concat(...subBreeds);
     console.log(data);
     return data;
   } catch (error) {
@@ -30,8 +37,12 @@ async function getData(URL) {
 
 async function getImage(URL) {
   try {
-    const response = await fetch(URL);
-    return response;
+    const response = await fetch(
+      `https://dog.ceo/api/breed/${URL}/images/random`
+    );
+    const image = await response.json();
+    console.log(`status code: ${response.status}`);
+    return image.message;
   } catch (error) {
     console.log(error);
   }
@@ -39,21 +50,49 @@ async function getImage(URL) {
 
 async function init(x) {
   let promise = await x;
+  let response = promise.response;
   return promise;
 }
 
-getData(ApiLinks.Breeds);
-getData("https://dog.ceo/api/breed/husky/images/random");
-console.log(getData(ApiLinks.Breeds));
+function startUp() {
+  getData(ApiLinks.Breeds).then((data) => {
+    data.forEach((breed) => {
+      parentInserter(breed);
+    });
+  });
+}
+
+async function parentInserter(breed) {
+  let image = await getImage(breed);
+  insertHTML(breed, image);
+}
+
+function clear() {
+  DOMSelectors.parent.innerHTML = "";
+  DOMSelectors.input.value = "";
+}
+
+startUp();
 
 DOMSelectors.button.addEventListener("click", function (event) {
-  getData(ApiLinks.Breeds);
-  //console.log(data);
-  console.log(ApiLinks.Images);
-  getImage(ApiLinks.Images);
-  insertHTML(getData(ApiLinks.Breeds), getImage(ApiLinks.Images));
-  clearInput();
+  const breed = DOMSelectors.input.value.toLowerCase();
+  console.log(breed);
+  getData(ApiLinks.Breeds).then((data) => {
+    console.log(data);
+    if (data.includes(breed)) {
+      parentInserter(breed);
+    } else {
+      DOMSelectors.parent.innerHTML = DOMSelectors.errorHTML;
+    }
+  });
+  clear();
   event.preventDefault();
 });
 
-console.log(ApiLinks.Images);
+async function hello() {
+  let hello = await fetch`https://dog.ceo/api/breed/irish/images/random`;
+  let hi = await hello;
+  console.log(hi);
+}
+
+hello();
